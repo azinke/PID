b7d8bb5a03ed032c9a2f1d3a357464455891f515
AMOUSSOU Z. Kenneth <kenneth.amoussou@gmail.com>
2018-10-06 15:13:56 +0100

    Fix: example code for LED dimming with PID through PWM

---
+++ example/ledDimming/ledDimming.ino
@@ -0,0 +1,44 @@
+#include <PID.h>
+
+PID myPID(1, 0.5, 0.5);
+
+/* init variable */
+float set_point = 0.0;
+float measure = 0.0;
+float output = 0.0;
+
+uint8_t val = 0;
+
+/* function prototype */
+uint8_t tf(float volt);
+
+void setup(){
+    Serial.begin(9600);
+    
+    myPID.setLimits(0, 255);    // set upper and lower limit of the controller
+    
+    pinMode(9, OUTPUT);
+}
+
+void loop(){
+
+    /* measured output */
+    measure = (analogRead(A1) * 5)/1023; // sensor - photoresistor
+    
+    output = myPID.getOutput(measure);
+    //Serial.println(output);
+    
+    /* update output */
+    val = tf(output);
+    Serial.println("val: " + String(val));
+    analogWrite(9, val);
+    
+    /* Reference */
+    set_point = (analogRead(A0) * 5)/1023; // fixed reference
+    myPID.setPoint(set_point);
+    
+}
+
+uint8_t tf(float volt){
+    return (volt * 255)/5;
+}
